8e09be48dc8d2bbfa2157dd57a90918118e76220
ikramhasib007 <ikramhasib007@gmail.com>
2018-11-25 00:46:43 +0600

    fetching data for pages

--- pages/post.js
+++ pages/post.js
@@ -1,18 +1,20 @@
-import {withRouter} from 'next/router';
 import Layout from '../components/MyLayout';
-import {Fragment} from 'react';
 
-const Content = withRouter((props) => (
-    <Fragment>
-        <h1>{props.router.query.title}</h1>
-        <p>This is the blog content page.</p>
-    </Fragment>
-))
-
-const Page = () => (
+const Post = (props) => (
     <Layout>
-        <Content/>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g,'')}</p>
+        <img src={props.show.image.medium} alt={props.show.name}/>
     </Layout>
-)
+);
 
-export default Page;
+Post.getInitialProps = async function(context) {
+    const {id} = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+    return {show};
+}
+
+export default Post;
