9647135c5ed77d8d1c4ca98e6b56cca7e6752f5d
ikramhasib007 <ikramhasib007@gmail.com>
2018-11-24 21:22:12 +0600

    navigation using anchor tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+import Router from 'next/router';
+
 const Index = () => (
     <div>
+        <Link href="/about">
+            <button style={{border: "none"}}>Got to the about page</button>
+        </Link>
+        <br/>
+        <Link scroll={false} href={{pathname: "about", query:{counter:"10"}}}><a>Disables scrolling</a></Link>
+
+        Click <span onClick={() => Router.push('/about')}>here</span> to read more
         <h1>Hello Next.js</h1>
     </div>
 )
